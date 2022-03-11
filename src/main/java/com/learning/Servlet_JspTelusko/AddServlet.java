4d573cc09a6e709d728e38c43584ccb883eef64d
rupesss-09 <rupesss09@gmail.com>
2022-03-10 21:13:03 -0500

    Sending response to browser instead of printing in console.

--- src/main/java/com/learning/Servlet_JspTelusko/AddServlet.java
+++ src/main/java/com/learning/Servlet_JspTelusko/AddServlet.java
@@ -3,15 +3,19 @@ package com.learning.Servlet_JspTelusko;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class AddServlet extends HttpServlet {
 
-    public void service(ServletRequest req, ServletResponse res) {
+    public void service(ServletRequest req, ServletResponse res) throws IOException {
         int i = Integer.parseInt(req.getParameter("num1"));
         int j = Integer.parseInt(req.getParameter("num2"));
 
         int k = i + j;
 
-        System.out.println(k);
+        PrintWriter out = res.getWriter();
+        out.println("Result is now:" + k);
+//        res.getWriter().println("Result is :" + k);
     }
 }
