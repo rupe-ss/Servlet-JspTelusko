5e33827459bd784a1b38ffdc65c18c9cf64ebd42
rupesss-09 <rupesss09@gmail.com>
2022-03-15 23:48:28 -0400

    COOKIES and implementation - One server sending data in form of cookies and same data is send by browser in form of COOKIES

--- src/main/java/com/learning/Servlet_JspTelusko/AddServlet.java
+++ src/main/java/com/learning/Servlet_JspTelusko/AddServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.HttpCookie;
 
 public class AddServlet extends HttpServlet {
 
@@ -18,26 +19,31 @@ public class AddServlet extends HttpServlet {
         String projectName = "Session Topic";
         int k = i + j;
 
-//      PrintWriter out = resp.getWriter();
-//      out.println("Result is now:" + k);
-//      resp.getWriter().println("Result is :" + k);
+      /*PrintWriter out = resp.getWriter();
+      out.println("Result is now:" + k);
+      resp.getWriter().println("Result is :" + k);*/
 
-//      We have to set attribute, so that we can send some value to another servlet. We can do that by setting attribute.
-//        req.setAttribute("k", k);
-//        RequestDispatcher rd = req.getRequestDispatcher("sq");
-//        rd.forward(req, resp);
+        /*//We have to set attribute, so that we can send some value to another servlet. We can do that by setting attribute.
+        req.setAttribute("k", k);
+        RequestDispatcher rd = req.getRequestDispatcher("sq");
+        rd.forward(req, resp);*/
 
-        //We are managing with URL Redirect
-//        resp.sendRedirect("sq");
+        /*//We are managing with URL Redirect
+        resp.sendRedirect("sq");
         //We can send data as well
-//        resp.sendRedirect("sq?k="+9);
+        resp.sendRedirect("sq?k="+9);*/
 
-        //Using session to store data so we can use in another Servlet
+        /*//Using session to store data so we can use in another Servlet
         HttpSession session = req.getSession();
         session.setAttribute("k", k);
         session.setAttribute("projectName", projectName);
-
         //When you redirect it will send to different path, here path will be "localhost:8080/sq"
+        resp.sendRedirect("sq");*/
+
+        //Using cookies to store data so we can send back to browser and from there it will be send to another Servlet
+        //Below k is integer so we are changing integer to string by attaching empty string to it.
+        Cookie cookie = new Cookie("k", k + "");
+        resp.addCookie(cookie);
         resp.sendRedirect("sq");
     }
 
--- src/main/java/com/learning/Servlet_JspTelusko/SqServlet.java
+++ src/main/java/com/learning/Servlet_JspTelusko/SqServlet.java
@@ -3,10 +3,7 @@ package com.learning.Servlet_JspTelusko;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -14,15 +11,16 @@ public class SqServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // Since, attribute is object so we can convert easily to integer.
+
+        /*// Since, attribute is object so we can convert easily to integer.
         // We don't have to use Integer.parse() method to convert object to integer
-//        int k = (int) req.getAttribute("k");
-//        k = k * k;
+        int k = (int) req.getAttribute("k");
+        k = k * k;*/
 
-//        PrintWriter out = resp.getWriter();
-//        out.println("Testing request dispatcher, result is:");
+        /*PrintWriter out = resp.getWriter();
+        out.println("Testing request dispatcher, result is:");*/
 
-        HttpSession session = req.getSession();
+        /*HttpSession session = req.getSession();
         String projectName = (String) session.getAttribute("projectName");
 
         session.removeAttribute("k");
@@ -34,6 +32,20 @@ public class SqServlet extends HttpServlet {
             out.println("Testing sendRedirect, result is:" + k + ".\n" + "Project Name is:-" + projectName);
         } else {
             out.println("Attribute \"k\" is removed only project name is available" + ".\n" + "Project Name: " + projectName);
+        }*/
+
+        //So when client is sending cookies to Server, its sending lot of cookies,
+        // First we have to recieve the cookies
+        int k = 0;
+        Cookie cookies[] = req.getCookies();
+        for( Cookie c : cookies){
+            if( c.getName().equals("k")){
+                k = Integer.parseInt(c.getValue());
+            }
         }
+        resp.getWriter().println("The sum of two number is integer:" + k);
+
+        //Below line is printing all html element tag as well need to find out how tag won't show on browser page
+        resp.getWriter().println("<html><body><h1> AddServlet created a cokie name \"k\" and we recieved that cookie in SqServlet</h1></body></html>");
     }
 }
